5ee709ca807068fc146a3b587dcfac248ff5a6f1
jookebawx <110410554+jookebawx@users.noreply.github.com>
2023-05-15 19:55:51 +0900

    Add files via upload

--- tx.py
+++ tx.py
@@ -1,26 +1,55 @@
 import json
+import base58
+import hashlib
 
 class Transaction:
 
-    def __init__(self, tx_type, address, pub_key, balance, data):
+    def __init__(self, tx_type, address, balance, data):
         self.type= tx_type
-        self.address = address
-        self.pub_key = pub_key
-        self.balance = balance
-        self.data = data
-
+        if self.verify_wallet_address(address):
+            self.address = address
+            self.balance = balance
+            self.data = data
+            self.hash = self.generate_hash()
+        else:
+            print("invalid")            
+        
+    
     def to_json(self):
         return json.dumps({
         "type" : self.type,
-        "address" : self.address,
-        "pub_key" : self.pub_key,
+        "address" : str(self.address),
         "balance": self.balance,
         "data" : self.data, 
+        "tx_hash": self.hash
         },  indent=2,sort_keys=True, ensure_ascii = False)
     
-    def verify_address(self, address):
-        if address[2:] != "sc" and len(address)!=20:
-            print("ADDRESS IS NOT VALID")
-    
+    def generate_hash(self):
+        tx_content = self.type + str(self.balance) + self.data
+        h = hashlib.sha256(tx_content.encode()).hexdigest()
+        return h
+
+    def verify_wallet_address(self, address):
+        # Decode the wallet address from Base58
+        try:
+            decoded_address = base58.b58decode(address)
+        except ValueError:
+            return False
+
+        # Split the decoded address into the hash and the checksum
+        checksum = decoded_address[-4:]
+        # Calculate the double SHA256 hash of the extended hash
+        hash = hashlib.sha256(decoded_address[:-4]).digest()
+        hash = hashlib.sha256(hash).digest()
+        # Get the first 4 bytes of the double SHA256 hash (the checksum)
+        expected_checksum = hash[:4]
+        # Verify that the expected checksum matches the actual checksum
+        return checksum == expected_checksum
+
+        # Verify that the network prefix matches the expected prefix
+        # if hash.startswith(b'sc'):
+        #     return True
+        # else:
+        #     return False
 
  
